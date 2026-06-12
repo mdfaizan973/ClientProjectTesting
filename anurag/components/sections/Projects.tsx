2841ff39d1a3126b2a077614f1cbf22bb9e9df74
Md Faizan <faizan.md9735@gmail.com>
2026-06-12 17:46:29 +0530

    -

--- anurag/components/sections/Projects.tsx
+++ anurag/components/sections/Projects.tsx
@@ -56,7 +56,7 @@ export default function Projects() {
               variants={containerVariants}
               className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
             >
-              {PORTFOLIO_DATA.projects.map((project) => (
+              {PORTFOLIO_DATA?.projects?.map((project) => (
                 <motion.a
                   key={project.id}
                   href={project.link}
